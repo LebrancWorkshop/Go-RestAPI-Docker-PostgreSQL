d120ee2490fc8e28cdebd1151d71cae865d91763
lebrancconvas <lebrancconvas@gmail.com>
2022-12-31 20:30:20 +0700

    🎉 feat(init): init project

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main 
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func getIndex(c *fiber.Ctx) error {
+	return c.SendString("Test API."); 
+}
+
+func main() {
+	app := fiber.New();
+	app.Get("/", getIndex);
+	app.Listen(":8000");
+}
